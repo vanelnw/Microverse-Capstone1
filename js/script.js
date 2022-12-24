8dcd891dc3e96c4663ad37b70766875a2c063aa5
vanelnw <vanelnwaba@gmail.com>
2022-12-24 04:53:36 +0100

    Refactoring  the code

--- js/script.js
+++ js/script.js
@@ -1,125 +1,122 @@
-const speakers = [
+const doctors = [
   {
-    image: "./assets/doctor1.jpg",
-    name: "Dr.Tom Stewart",
-    organization: "Leading Neurologist",
+    image: './assets/doctor1.jpg',
+    name: 'Dr.Tom Stewart',
+    organization: 'Leading Neurologist',
     description:
-      "Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.",
+      'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
   },
   {
-    image: "./assets/doctor3.jpg",
-    name: "Dr. Anna Walker",
-    organization: "Traumatologist",
+    image: './assets/doctor3.jpg',
+    name: 'Dr. Anna Walker',
+    organization: 'Traumatologist',
     description:
       "As the author of <Digital Art Art of Our Time>, he opened 'Art Center Nabi', Korea's first digital art institution in 2000, and is currently serving.",
   },
   {
-    image: "./assets/doctor2.jpg",
-    name: "Dr. Paul Ford",
-    organization: "Cardiac Surgeon",
+    image: './assets/doctor2.jpg',
+    name: 'Dr. Paul Ford',
+    organization: 'Cardiac Surgeon',
     description:
       "By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990.",
   },
 
   {
-    image: "./assets/doctor5.jpg",
-    name: "Dr. Sarah Snow",
-    organization: "Pediatrician",
+    image: './assets/doctor5.jpg',
+    name: 'Dr. Sarah Snow',
+    organization: 'Pediatrician',
     description:
-      "Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia. Wikipedia is provided free of charge in 290 languages ​.",
+      'Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia. Wikipedia is provided free of charge in 290 languages ​.',
   },
   {
-    image: "./assets/doctor4.jpg",
-    name: "Dr. Andrew Green",
-    organization: "Dermatologist",
+    image: './assets/doctor4.jpg',
+    name: 'Dr. Andrew Green',
+    organization: 'Dermatologist',
     description:
       "European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU's copyright law in July.",
   },
 
   {
-    image: "./assets/doctor6.jpg",
-    name: "Dr. Sandra Wok",
-    organization: "Gynicologist",
+    image: './assets/doctor6.jpg',
+    name: 'Dr. Sandra Wok',
+    organization: 'Gynicologist',
     description:
-      "He led open source projects at the Mozilla Foundation and joined CC as CEO in 2014. He has been active in open movements such as open government and open source.",
+      'He led open source projects at the Mozilla Foundation and joined CC as CEO in 2014. He has been active in open movements such as open government and open source.',
   },
 ];
 
-
-
-const closeMenu = document.querySelector(".close-menu");
-const nav = document.querySelector("nav");
-const burgerMenu = document.querySelector(".mobile-menu");
+const closeMenuButton = document.querySelector('.close-menu');
+const nav = document.querySelector('nav');
+const burgerMenuButton = document.querySelector('.mobile-menu');
 
 function toggleMenu() {
-  nav.classList.toggle("nav-show");
+  nav.classList.toggle('nav-show');
 }
 
-if (burgerMenu) {
-  burgerMenu.addEventListener("click", toggleMenu);
+if (burgerMenuButton) {
+  burgerMenuButton.addEventListener('click', toggleMenu);
 }
 
-if (closeMenu) {
-  closeMenu.addEventListener("click", toggleMenu);
+if (closeMenuButton) {
+  closeMenuButton.addEventListener('click', toggleMenu);
 }
 
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("nav-item")) {
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('nav-item')) {
     toggleMenu();
   }
 });
 
-const body = document.querySelector("body");
-const speakersSection = document.createElement("div");
+const doctorsSection = document.querySelector('.doctors');
 
-speakersSection.setAttribute("class", "section speakers");
+const doctorsSectionTitle = document.createElement('div');
+doctorsSectionTitle.classList.add('title', 'doctors-title');
+doctorsSectionTitle.innerHTML = '<span>Our doctors</span>';
 
-const speakersSectionTitle = document.createElement("div");
-speakersSectionTitle.setAttribute("class", "title speakers-title");
-speakersSectionTitle.innerHTML = "<span>Featred speakers</span>";
+const doctorsSectionContent = document.createElement('div');
+doctorsSectionContent.classList.add('content', 'doctors-content');
 
-const speakersSectionContent = document.createElement("div");
-speakersSectionContent.setAttribute("class", "content speakers-content");
-
-function AppendSpeakerItem(Speakers) {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const element of Speakers) {
-    const speakerItem = document.createElement("div");
-    speakerItem.setAttribute("class", "speaker-item");
-    speakerItem.innerHTML = `<img class="speaker-item-img" src=${element.image} alt="speaker1" />
-            <div class="speaker-item-info">
-            <h4 class="speaker-name">${element.name}</h4>
-                      <p class="speaker-organize">${element.organization}</p>
-                      <hr />
-                      <p class="speaker-desc">
-                      ${element.description}
-                      </p>
-            </div>
-          `;
-
-    speakersSectionContent.append(speakerItem);
-  }
+function createDoctorItem(doctor) {
+  const doctorItem = document.createElement('div');
+  doctorItem.classList.add('doctor-item');
+  doctorItem.innerHTML = `
+    <img class="doctor-item-img" src=${doctor.image} alt="doctor" />
+    <div class="doctor-item-info">
+      <h4 class="doctor-name">${doctor.name}</h4>
+      <p class="doctor-organize">${doctor.organization}</p>
+      <hr />
+      <p class="doctor-desc">${doctor.description}</p>
+    </div>
+  `;
+  return doctorItem;
 }
 
-AppendSpeakerItem(speakers);
+function appendDoctorItems(doctors) {
+  if (!Array.isArray(doctors) || !doctors.length) {
+    return;
+  }
+  doctors.forEach((doctor) => {
+    const doctorItem = createDoctorItem(doctor);
+    doctorsSectionContent.append(doctorItem);
+  });
+}
 
-const morebtn = document.createElement("a");
-morebtn.setAttribute("class", "more-btn");
-morebtn.innerText = "more";
+appendDoctorItems(doctors);
 
-morebtn.addEventListener("click", () => {
-  const content = document.querySelector(".speakers-content");
+const moreButton = document.createElement('a');
+moreButton.setAttribute('class', 'more-btn');
+moreButton.innerText = 'more';
 
+moreButton.addEventListener('click', () => {
+  const content = document.querySelector('.doctors-content');
   const h = content.offsetHeight;
-
-  speakersSectionContent.style.maxHeight = `${h * 2}px`;
+  doctorsSectionContent.style.maxHeight = `${h * 2}px`;
 
   if (content.offsetHeight >= 1100) {
-    morebtn.classList.add("more-btn-none");
+    moreButton.classList.add('more-btn-none');
   }
 });
 
-speakersSection.append(speakersSectionTitle);
-speakersSection.append(speakersSectionContent);
-speakersSection.append(morebtn);
-body.insertBefore(speakersSection, body.children[2]);
+doctorsSection.append(doctorsSectionTitle);
+doctorsSection.append(doctorsSectionContent);
+doctorsSection.append(moreButton);
